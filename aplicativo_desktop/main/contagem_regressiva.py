a7ee5e8dfac929b7c5ff9a7c9975e8bdc9ce4b18
Erick Bryan Cubas <thecubas@outlook.com>
2023-08-19 06:37:03 -0300

    pooh

--- aplicativo_desktop/main/contagem_regressiva.py
+++ aplicativo_desktop/main/contagem_regressiva.py
@@ -6,8 +6,9 @@ import pygame
 
 caminho_usuario = os.environ['USERPROFILE']
 musica = 'floating-cat.mp3'
+pooh = 'pooh.gif'
 caminho_musica = os.path.join(caminho_usuario, 'Área de Trabalho', 'Projetos', 'Contador_Maria', 'aplicativo_desktop', 'music', musica)
-
+caminho_gif = os.path.join(caminho_usuario, 'Área de Trabalho', 'Projetos', 'Contador_Maria', 'aplicativo_desktop', 'images', pooh)
 
 # Define a target date for the countdown
 data_alvo = datetime.datetime(2023, 9, 7)
@@ -17,29 +18,21 @@ def calcular_tempo_restante():
     tempo_restante = data_alvo - tempo_atual
     return tempo_restante
 
-def atualizar_contagem_regressiva(label_detalhada, label_hora):
+def atualizar_contagem_regressiva(label_detalhada):
     tempo_restante = calcular_tempo_restante()
 
     if tempo_restante.days < 0:
         label_detalhada.config(text="A data alvo já foi alcançada!")
-        label_hora.config(text="00:00:00")
     else:
         total_seconds = int(tempo_restante.total_seconds())
         dias, remainder = divmod(total_seconds, 86400)
         horas, remainder = divmod(remainder, 3600)
         minutos, segundos = divmod(remainder, 60)
 
-        mensagem_detalhada = "Faltam {} dias, {} horas, {} minutos e {} segundos, meu Lírio 🌸".format(dias, horas, minutos, segundos)
+        mensagem_detalhada = "Faltam {} dias, {} horas, {} minutos e {} segundos, meu Pãozinho de Mel 🍯".format(dias, horas, minutos, segundos)
         label_detalhada.config(text=mensagem_detalhada)
 
-        total_horas = total_seconds // 3600
-        remainder = total_seconds % 3600
-        minutos, segundos = divmod(remainder, 60)
-        mensagem_hora = "{:02d}:{:02d}:{:02d}".format(total_horas, minutos, segundos)
-        label_hora.config(text=mensagem_hora)
-
-    label_detalhada.after(1000, lambda: atualizar_contagem_regressiva(label_detalhada, label_hora))
-    label_hora.after(1000, lambda: atualizar_contagem_regressiva(label_detalhada, label_hora))
+    label_detalhada.after(1000, lambda: atualizar_contagem_regressiva(label_detalhada))
 
 def criar_interface():
     pygame.init()
@@ -47,25 +40,26 @@ def criar_interface():
     janela = tk.Tk()
     janela.title("❤️Contagem Regressiva Para Te Ver❤️")
     
-    # Alterando a cor de fundo
-    janela.configure(bg='#e63946')
+    # Carregar e exibir o GIF
+    imagem_pooh = tk.PhotoImage(file=caminho_gif)
+    gif_label = tk.Label(janela, image=imagem_pooh, bg='#ffc300')
+    gif_label.pack()
     
-    fonte_hora = ("Digital-7", 18)
+    # Definir o tamanho da janela para o tamanho do GIF
+    janela.geometry("{}x{}".format(imagem_pooh.width(), imagem_pooh.height()))
+
+    # Alterando a cor de fundo
+    janela.configure(bg='#ffc300')
+    
     fonte_detalhada = ("Helvetica Bold", 16)
 
     frame_detalhada = tk.Frame(janela, relief="groove")
     frame_detalhada.pack(pady=1)
 
-    label_detalhada = tk.Label(frame_detalhada, font=fonte_detalhada, padx=20, pady=20, bg='#e63946', fg='white')
+    label_detalhada = tk.Label(frame_detalhada, font=fonte_detalhada, padx=20, pady=20, bg='#ffc300', fg='white')
     label_detalhada.pack()
 
-    frame_hora = tk.Frame(janela, bd=5, relief="groove", bg='black')
-    frame_hora.pack(pady=10)
-
-    label_hora = tk.Label(frame_hora, font=fonte_hora, bg='white', fg='#e63946', bd=2, relief="groove", padx=20, pady=20)
-    label_hora.pack()
-
-    atualizar_contagem_regressiva(label_detalhada, label_hora)
+    atualizar_contagem_regressiva(label_detalhada)
     
     pygame.mixer.music.load(caminho_musica)
     pygame.mixer.music.play(-1)
